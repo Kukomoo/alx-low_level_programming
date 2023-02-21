85374f1af0c264620be08b4233a6c95edbabb276
Kukomoo <Nad1071996@outlook.com>
2023-02-21 13:51:21 -0800

    yay

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - check the code
+ * _islower - used to check if character is lowcase
+ * int c is used to declare c
+ *
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+	int y = 1;
+	int n = 0;
+
+	if (c <= 'z' && c >= 'a')
+	_putchar(y);
+	else
+{
+	_putchar(n);
+	}
+}
