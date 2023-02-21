690076587736b1fbbb875bb0a6e63fbc2d179d92
Kukomoo <Nad1071996@outlook.com>
2023-02-21 15:21:29 -0800

    yay

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int _isalpha(int c)
+{
+	if (( c >= 65 && c <= 90 ) || ( c >= 97 && c <= 112))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
