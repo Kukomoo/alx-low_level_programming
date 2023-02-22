453ce1b420fe8b8bc0b3793eed4287b0466834c2
Kukomoo <Nad1071996@outlook.com>
2023-02-22 11:53:54 -0800

    yay

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,11 @@
 #include "main.h"
  /**
  *  main - check the code
- *  print_alphabet - decleration
+ *  print_alphabet - Prints "a-z"
  *
  * Return: Always 0.
  */
-void print_alphabet(void)/**decleration found in main**/
+void print_alphabet(void)
 {
 char alpha;
 
