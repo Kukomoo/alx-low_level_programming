614b4738706b114f2f9b4c34c3e27e6aa6ac6c31
Kukomoo <Nad1071996@outlook.com>
2023-02-23 20:53:11 +0000

    yay

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,10 +6,10 @@
 void print_most_numbers(void)
 {
 	int i;
-
-                for (i = 0; i <= 9; i++)
-		{
-			if ((i == 2)||(i == 4))
+	
+	for (i = 0; i <= 9; i++)
+	{
+			if ((i == 2) || (i == 4))
 			{
 				continue;
 			}
@@ -17,6 +17,6 @@ void print_most_numbers(void)
 			{
 				_putchar(i + '0');
 			}
-                }
-                _putchar('\n');
+	}
+	_putchar('\n');
 }
