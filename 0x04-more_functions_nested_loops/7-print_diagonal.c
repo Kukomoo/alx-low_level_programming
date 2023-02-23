af1bfb8cbe8fd56b2ea360c5f6c4d7ae2a54d9e1
Kukomoo <Nad1071996@outlook.com>
2023-02-23 23:06:05 +0000

    yay

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -17,10 +17,10 @@ else
 {
 	for (count = 0; count < n; count++)
 	{
-		for (space = 0; space < count; space++)
-		{
-		      _putchar(32);
-		}
+for (space = 0; space < count; space++)
+{
+       	_putchar(32);
+}
 	_putchar(92);
 	_putchar('\n');
 	}
