1869e2941d2c8c98b1401b475fbdd0f838a90091
Kukomoo <Nad1071996@outlook.com>
2023-02-23 16:24:43 +0000

    yay

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /*
- * int _isupper - this is the function name
+ * _isupper - this is the function name
  * description - it checks for uppercsdr character
  * @c - first opreator
  * Return: 1 if c is upper case else 0
