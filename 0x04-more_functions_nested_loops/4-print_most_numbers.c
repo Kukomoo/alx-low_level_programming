6881d78dae14e4b6c8359a030bbe5070d7ebf04f
Kukomoo <Nad1071996@outlook.com>
2023-02-23 20:37:31 +0000

    yay

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,11 +5,11 @@
  */
 void print_numbers(void)
 {
-        int i;
+        int i = 0;
 
-                for (i = 0; i <= 9; i++)
-                {
-			if ( i == 2||i == 4)
+                for (i <= 9; i++)
+i                {
+			if (i == 2||i == 4)
 			{
 				continue;
 			}
