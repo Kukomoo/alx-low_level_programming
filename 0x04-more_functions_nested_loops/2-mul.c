1c3b536126e2f190b3fbb8b3f31e0ae7ed7e30a2
Kukomoo <Nad1071996@outlook.com>
2023-02-23 18:07:38 +0000

    yay

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,6 +3,8 @@
  * mul - this is the function name
  * description - it multiplies two integers
  * Return: result of multiplication
+ * @a: first operand
+ * @b: second operand
  */
 
 int mul(int a, int b)
