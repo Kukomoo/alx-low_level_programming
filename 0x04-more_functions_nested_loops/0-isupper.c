cd70315a9a2a7d537c36fe9c54cec31fe928c039
Kukomoo <Nad1071996@outlook.com>
2023-02-23 16:28:41 +0000

    yay

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /*
- * _isupper - this is the function name _isupper
- * description - _isupper checks for uppercase character
+ * _isupper - _isupper checks for uppercase character
  * @c: first operand
  * Return: 1 if c is upper case else 0
  */
