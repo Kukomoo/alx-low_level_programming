229a67d2310e1f86319a5a94318f6975be2d47b6
Kukomoo <Nad1071996@outlook.com>
2023-02-23 19:55:58 +0000

    yay

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,15 +3,15 @@
  * print_numbers - this is the function name
  * description - this function prints from 0 to 9
  */
-void print_numbers(void);
+void print_numbers(void)
 {
-	int i = 0
-	{
+	int i = 0;
+
 		while (i < 10)
 		{
 			_putchar("%d", i);
 			_putchar("\n");
 			i++;
 		}
-	}
+
 }
