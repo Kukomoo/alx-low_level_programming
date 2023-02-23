7d3c01fbca9075725492e3359d802708315a2a52
Kukomoo <Nad1071996@outlook.com>
2023-02-23 20:43:48 +0000

    yay

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,20 +3,16 @@
  * print_numbers - this is the function name
  * description - this function prints from 0 to 9
  */
-void print_numbers(void)
+void print_numbers_numbers(void)
 {
 	int i;
 
                 for (i = 0; i <= 9; i++)
 		{
 			if ((i == )2||(i == 4))
-			{
 				continue;
-			}
 			else
-			{
-				_putchar(i + '0');
-			}	
+				_putchar(i + '0');	
                 }
                 _putchar('\n');
 }
