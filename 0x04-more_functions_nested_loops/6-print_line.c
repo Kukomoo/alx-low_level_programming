25c75f9611684f1005eeec904cf4c8fa34486f8b
Kukomoo <Nad1071996@outlook.com>
2023-02-23 22:40:13 +0000

    yay

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_line - the function name
+ * description - this draws a line
+ */
+
+void print_line(int n)
+{
+	int count;
+	
+	if (n <= 0)
+{
+	_putchar('\n');
+}
+else
+{
+	for (count = 0; count < n; count++)
+	{
+		_putchar(95);
+	}
+	_putchar('\n');
+}
+}
+
