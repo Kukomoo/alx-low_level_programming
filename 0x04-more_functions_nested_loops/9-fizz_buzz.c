790bed0422d6c04ed9d425f2133d093aa9dc6b68
Kukomoo <Nad1071996@outlook.com>
2023-02-24 00:02:19 +0000

    yay

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - entry point
+ * funtion - description fuzz game
+ * Return: 0 if succsses 
+ */
+int main (void)
+{
+	int i;
+	if (i = 1; i < 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else 
+		{
+			printf("%d", i);
+		}
+		if (i != 100)
+		{
+		printf(" ");
+		}
+	}
+printf("\n");
+return (0);
+}
