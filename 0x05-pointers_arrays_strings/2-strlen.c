cd172d59134aaa6eaac6db7ed37c1bcbd4e00f8c
Kukomoo <Nad1071996@outlook.com>
2023-02-27 23:12:08 +0000

    ayy

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/*
+ * _strlen - function 
+ * description - return the length of a string
+ * @s: pointer
+ */
+int _strlen(char *s)
+{
+	int string = 0;
+	while (*s != '\0')
+	{
+		string++;
+		s++;
+	}
+	return (length);
+}
