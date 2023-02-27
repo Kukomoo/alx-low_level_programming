5187152e90687392f8ad1122d7a15c70da37abf1
Kukomoo <Nad1071996@outlook.com>
2023-02-27 22:52:56 +0000

    ayy

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,13 +2,9 @@
 /**
  * reset_to_98 - function name
  * @*n: pointer
- * decription - this updated an int value to 98
+ * decription - this updates int value to 98
  */
 void reset_to_98(int *n)
 {
-int n;
-int *p = &n;
-*p = 98;
-_putchar(n);
-return (0);
+	*n = 98;
 }
