83fc94a27da4dcb73165268a01bf63c842c94af8
Kukomoo <Nad1071996@outlook.com>
2023-02-27 23:15:44 +0000

    ayy

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
- * _strlen - function name 
+ * _strlen - function name
  * description - return the length of a string
  * @s: pointer
+ * Return: the lenght of a string
  */
 int _strlen(char *s)
 {
