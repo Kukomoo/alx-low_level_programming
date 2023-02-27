fa14de42d0bc0d286486ee2318de7975b295045e
Kukomoo <Nad1071996@outlook.com>
2023-02-27 23:40:32 +0000

    ayy

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * rev_string - function name
+ * description - reverses a string
+ * @s: pointer
+ */
+void rev_string(char *s);
+{
+	int string = 0;
+	int x;
+	char null;
+
+	while (s[string] != '\0')
+		string++;
+	for (x = 0; x < string / 2; x++)
+	{
+		null = s[x]
+		s[x] = s[string - x - 1];
+		s[string - x - 1] = null;
+	}
