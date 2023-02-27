13de5f6eb3e9e88258e31d8542eb39896a75c795
Kukomoo <Nad1071996@outlook.com>
2023-02-27 23:14:12 +0000

    ayy

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,12 +1,13 @@
 #include "main.h"
-/*
- * _strlen - function 
+/**
+ * _strlen - function name 
  * description - return the length of a string
  * @s: pointer
  */
 int _strlen(char *s)
 {
 	int string = 0;
+
 	while (*s != '\0')
 	{
 		string++;
