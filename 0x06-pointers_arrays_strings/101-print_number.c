47adb846419406ad11e0721a757c687b10cafef7
Kukomoo <Nad1071996@outlook.com>
2023-03-02 23:11:03 +0000

    Q.9

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_number - function name
+ * description - prints an integer
+ * @n: number of integer
+ */
+void print_number(int n);
+{
+	unsigned int y;
+
+	y = n;
+	if (n < 0)
+	{
+	_putchar('-');
+	y = -n;
+	}
+	if (y / 10 != 0)
+	{
+	print_number(number / 10);
+	}
+	_putchar((y % 10) + '0');
+}
