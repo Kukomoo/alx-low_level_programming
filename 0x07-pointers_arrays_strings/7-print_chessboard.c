0f896b3e70815889fbaa779fc88fffbe6b5ff532
Kukomoo <Nad1071996@outlook.com>
2023-03-06 23:10:50 +0000

    yay

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,9 +7,12 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	for (int x = 0; x < 8; x++)
+	int x;
+	int y;
+
+	for (x = 0; x < 8; x++)
 	{
-		for (int y = 0; y < 8; y++)
+		for (y = 0; y < 8; y++)
 		{
 			if (y == 7)
 			{
