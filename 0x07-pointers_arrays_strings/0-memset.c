61a17b3cbce56da61f69358fca839495136f3028
Kukomoo <Nad1071996@outlook.com>
2023-03-06 12:47:38 -0800

    fixing syntax

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _memset - Function name
  * @s: pointer to string
@@ -6,6 +7,7 @@
  * @n: number of bytes
  * Return: a pointer
  */
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	for (unsigned int x = 0; x < n; x++)
