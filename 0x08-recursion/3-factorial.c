cd0dba79b0168252c6c49ed9f3f4c179aff1376d
Kukomoo <Nad1071996@outlook.com>
2023-06-15 11:49:51 -0700

    Q.3 solution

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+/**
+ * factorial - Function name
+ * decription - returns factorial of a number
+ * @n: int variable
+ * Return: factorial of a given number
+ */
+#include "main.h"
+int factorial(int n);
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (n * factorial(n))
+}
