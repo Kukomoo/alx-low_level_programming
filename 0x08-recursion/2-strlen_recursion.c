e1400fa9816bcfd134ce64095973664cafe50ddb
Kukomoo <Nad1071996@outlook.com>
2023-06-15 08:05:16 -0700

    Q2. solution

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+/**
+ * _strlen_recursion - Function name
+ * decription - returns length of string
+ * @s: pointer to character
+ * Return count of characters 
+ */
+#include "main.h"
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+	return 0;
+	}
+	return 1 + _strlen_recursion(s + 1);
+}
