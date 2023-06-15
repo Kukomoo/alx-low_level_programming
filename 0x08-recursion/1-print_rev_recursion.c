d6ea9f6c8f0172863dc70e44d9b0f6c0ec4318f5
Kukomoo <Nad1071996@outlook.com>
2023-06-15 07:38:45 -0700

    added solution

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+/**
+ * _print_rev_recursion - Function name
+ * decription - prints a string of characters in reverse
+ * @s: pointer to character
+ * Return void
+ */
+#include "main.h"
+void _print_rev_recursion(char *s)
+{
+    if (s == '\0')
+    return;
+    
+    _print_rev_recursion(s + 1);
+    _putchar(*s);
+}
